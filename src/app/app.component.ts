a962a8f4f766a3c97279501e54e40319efff244a
CHAURASIYA MAHADEV <mahadev.chaurasiya@CHAURASIYAs-MacBook-Pro.local>
2023-05-19 09:04:37 +0530

    English character only support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eng-validator';
+  regex = /^[~`!@#$%^&*()_+=[\]\{}|;':",.\/<>?a-zA-Z0-9 -]+$/;
+
+  pasteOnlyEnglishEvnt(event: any){
+    let clipboardData = event.clipboardData || window.Clipboard;
+    let pastedText = clipboardData.getData('text');
+    console.log("Paste : "+pastedText);
+    if (this.regex.test(pastedText)) {
+         return true;
+     }
+  //   //e.preventDefault();
+     return false;
+  }
+
+   allowEnglishWord (e:any) { 
+    var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+     console.log("KeyPress : "+str);
+     if (this.regex.test(str)) {
+         return true;
+     }
+     //e.preventDefault();
+     return false;
+   }
+  
 }
